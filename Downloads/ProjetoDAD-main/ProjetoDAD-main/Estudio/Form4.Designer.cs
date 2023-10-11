5ce33c599ea5c18b63a96ce345241d3d7f1d4e8a
Sofia <cl202244@g.unicamp.br>
2023-10-11 16:30:25 -0300

    TERMINAR TURMA

---
+++ Downloads/ProjetoDAD-main/ProjetoDAD-main/Estudio/Form4.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Estudio
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCpfAluno = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblCpfAluno
+            // 
+            this.lblCpfAluno.AutoSize = true;
+            this.lblCpfAluno.Location = new System.Drawing.Point(62, 51);
+            this.lblCpfAluno.Name = "lblCpfAluno";
+            this.lblCpfAluno.Size = new System.Drawing.Size(30, 13);
+            this.lblCpfAluno.TabIndex = 0;
+            this.lblCpfAluno.Text = "CPF:";
+            this.lblCpfAluno.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(99, 51);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(100, 20);
+            this.txtCpf.TabIndex = 1;
+            this.txtCpf.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCpf_KeyPress);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.lblCpfAluno);
+            this.Name = "Form4";
+            this.Text = "Excluir Aluno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCpfAluno;
+        private System.Windows.Forms.TextBox txtCpf;
+    }
+}
---
+++ Downloads/ProjetoDAD-main/ProjetoDAD-main/Estudio/Form4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudio
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExcluirAluno_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void mskCpfAluno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+          
+        }
+
+        private void mskCpfAluno_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+           
+        }
+
+        private void txtCpf_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Aluno aluno = new Aluno(txtCpf.Text);
+
+            if (e.KeyChar == 13);
+            {
+                if (aluno.consultarAluno())
+                {
+                    MessageBox.Show("Aluno encontrado");
+                    if (aluno.excluirAluno())
+                    {
+                        MessageBox.Show("Aluno excluído com sucesso");
+                    }
+                    else { MessageBox.Show("Erro!");
+                    }
+                }
+            }
+        }
+    }
+}
